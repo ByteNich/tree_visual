ae7e19b76997d6260a8cf73077bfa29b447b91d1
ByteNich <143332887+ByteNich@users.noreply.github.com>
2024-06-13 13:14:08 +0300

    Update AVL-tree.js

--- js-files/AVL-tree.js
+++ js-files/AVL-tree.js
@@ -4,27 +4,27 @@ class AVLTree {
         this.x = x;
         this.y = y;
     }
-    
+ 
     addValue(value) {
         this.root = this.root.insert(value);
         this.root.setSpacing(value);
         this.setCoordinates();
     }
-
+ 
     checkTree() {
         this.root.checkTree();
     }
-
+ 
     setCoordinates() {
         this.root.setCoordinates(this.x, this.y);
     }
-
+ 
     printTree(value) {
         this.root.printNode(1, value);
     }
-
+ 
     searchInTree(value) {
         return this.root.searchNode(value, 1);
     }
-
+ 
 } 
